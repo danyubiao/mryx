9ab084634442ec6e6ccab3a33b614e4a65a76a3c
bai <you@example.com>
2020-10-19 21:27:30 +0800

    添加分类断言

--- page/fenlei_page.py
+++ page/fenlei_page.py
@@ -52,8 +52,14 @@ class SousuoPage(BasePage):
 class FenLei(BasePage):
 #封装定位器
     fenlei_location =(By.ID,"cn.missfresh.application:id/classifyTab")
+#获取分类断言定位
+    fenlei_duanyan_locator=(By.ID,"cn.missfresh.application:id/tv_category_name")
 
      #点击分类跳转
     def click_dianji(self):
-        self.driver.find_element(*self.fenlei_location).click()
+        self.click(self.fenlei_location)
 
+    def duanyan_fenlei(self):
+        element= self.driver.find_element(*self.fenlei_duanyan_locator)
+        text= element.text
+        return text
