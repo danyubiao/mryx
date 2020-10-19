cce5fa558ba314ef0b23db18784ae774eef426f7
2636419668@qq.com <2636419668@qq.com>
2020-10-19 23:17:04 +0800

    执行器调试

--- run/ling_runner.py
+++ run/ling_runner.py
@@ -11,6 +11,7 @@ from BeautifulReport import BeautifulReport
 from testcase.test_add_gwc import AddgwcTest
 from testcase.test_paixu import PaixuTest
 from testcase.test_sousuo import SousuoTest
+from testcase.test_ts_login import TishiTest
 """执行所有用例"""
 
 # 创建测试集
@@ -21,7 +22,9 @@ testb1 = PaixuTest("test_price")
 testb2 = PaixuTest("test_sales")
 testc1 = SousuoTest("test_clear_sousuo")
 testc2 = SousuoTest("test_sousuo")
-suite.addTests((testa1, testa2, testb1, testb2, testc1, testc2))
+testd1 = TishiTest("test_ts_login")
+testd2 = TishiTest("test_share_goods")
+suite.addTests((testa1, testa2, testb1, testb2, testc1, testc2, testd1, testd2))
 
 # 创建执行器
 runner = BeautifulReport(suite)
