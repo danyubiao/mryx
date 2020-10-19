9826c28aead0c51c4d5ac0e99f208854ed410336
2636419668@qq.com <2636419668@qq.com>
2020-10-19 21:49:20 +0800

    分享商品用例调试

--- page/ts_login_page.py
+++ page/ts_login_page.py
@@ -9,7 +9,7 @@ from appium.webdriver.common.mobileby import MobileBy as By
 
 
 class TsLoginPage(BasePage):
-    """提示登录页面封装"""
+    """MRYX_ST_classification_005封装"""
     """点击商品定位器"""
     goods_locator = (By.XPATH, "//android.view.View[@resource-id=\"cn.missfresh.application:id/recycler_view\"]/android.widget.RelativeLayout[3]")
     """点击立即购买定位器"""
@@ -27,5 +27,20 @@ class TsLoginPage(BasePage):
 
     def assert_login(self):
         ele = self.driver.find_element(*self.assert_login_locator)
+        text = ele.text
+        return text
+
+    """MRYX_ST_classification_010封装"""
+    """分享定位器"""
+    share_goods_locator = (By.ID, "cn.missfresh.application:id/img_share")
+    """断言分享定位器"""
+    dy_share_locator = (By.ID, "cn.missfresh.application:id/btn_share_cancle")
+    def share_goods(self):
+        """封装点击分享方法"""
+        self.driver.find_element(*self.share_goods_locator).click()
+
+    def assert_share(self):
+        """断言分享方法"""
+        ele = self.driver.find_element(*self.dy_share_locator)
         text = ele.text
         return text
--- testcase/test_ts_login.py
+++ testcase/test_ts_login.py
@@ -11,17 +11,28 @@ from page.ts_login_page import TsLoginPage
 from time import sleep
 
 class SousuoTest(BaseCase):
-    def test_ts_login(self):
-        """MRYX_ST_classification_001"""
-        """搜索测试"""
+    # def test_ts_login(self):
+    #     """MRYX_ST_classification_001"""
+    #     """搜索测试"""
+    #     sort = HomePage(self.driver)
+    #     sort.click_sort()               # 进入分类页面
+    #     ts = TsLoginPage(self.driver)
+    #     ts.click_goods()                # 点击商品
+    #     ts.click_buy()                  # 点击立即购买
+    #     sleep(3)
+    #     dy = ts.assert_login()          # 断言提示登录
+    #     self.assertEqual(dy, "登录")
+
+    def test_share_goods(self):
+        """MRYX_ST_classification_010"""
+        """分享测试"""
         sort = HomePage(self.driver)
         sort.click_sort()               # 进入分类页面
-        ts = TsLoginPage(self.driver)
-        ts.click_goods()                # 点击商品
-        ts.click_buy()                  # 点击立即购买
-        sleep(3)
-        dy = ts.assert_login()          # 断言提示登录
-        self.assertEqual(dy, "登录")
+        fx = TsLoginPage(self.driver)
+        fx.click_goods()                # 点击商品
+        fx.share_goods()                # 点击分享按钮
+        dy = fx.assert_share()          # 断言分享功能
+        self.assertEqual(dy, "取消")
 
 if __name__ == '__main__':
     unittest.main()
