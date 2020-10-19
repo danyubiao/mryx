c51252ea6e33d0f0f86b81b55ff614eac251f429
DYB <1598223151@qq.com>
2020-10-19 11:58:35 +0800

    修改dyb的run

--- run/dyb_run.py
+++ run/dyb_run.py
@@ -4,7 +4,14 @@
 # @Email : xgtlz@gmail.com
 # @File : dyb_run.py
 # @Project : mryx
-"""这是但钰彪的运行"""
+"""这是但钰彪的运行
+测试用例有
+    logout_login_test.py 登录注销
+    test_activity_news.py 活动消息
+    test_add_shopping_address.py 添加收货地址测试
+    test_customer_service.py 客服与服务的测试
+    test_points_mall.py 积分商城测试
+    """
 from conf.config import TESTCASE_PATH,REPORT_PATH
 import unittest
 import time
@@ -16,3 +23,4 @@ file_name = "mysx_dyb_"+strTime+".html"
 BeautifulReport(discover).report(description = "每日优鲜测试用例",
                                  log_path = REPORT_PATH,
                                  filename = file_name)
+
