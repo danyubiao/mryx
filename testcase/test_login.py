172cf7a73815dba9c24832acff269182a8e12d55
DYB <1598223151@qq.com>
2020-10-20 09:11:49 +0800

    修正测试用例

--- testcase/test_login.py
+++ testcase/test_login.py
@@ -20,11 +20,21 @@ class TestLogin(BaseCase):
     def setUp(self) -> None:
         hp = HomePage(self.driver)
         hp.click_mine()
-        mp = MysPage(self.driver)
-        mp.click_loging_or_registered() # 点击登录或注册
+        sleep(2)
+        hp.to_up(duration=3000)  # 上滑出现设置
+        mp = MysPage(self.driver)  # 实例化"我的"界面
+        mp.click_set()  # 点击设置按钮
+        sleep(1)
+        msp = MysSetPage(self.driver)  # 实例化我的设置界面
+        msp.click_sign_out()  # 点击退出登录
 
     def test_MRYX_ST_usr_001(self):
         """测试登录正确登录"""
+        hp = HomePage(self.driver)
+        hp.click_mine_test()
+        sleep(2)
+        mp = MysPage(self.driver)
+        mp.click_loging_or_registered()  # 点击登录或注册
         lorp = LoginOrRegisterPage(self.driver)
         lorp.send_phone_number(self.phone_number)  # 输入手机号码
         lorp.click_get_check_code()  # 点击获取验证码
@@ -37,15 +47,4 @@ class TestLogin(BaseCase):
         self.assertEqual(self.asserts1,text)
 
     def tearDown(self) -> None:
-        hp = HomePage(self.driver)
-        sleep(5)
-        hp.click_mine_test()  # 点击我的
-        sleep(2)
-        hp.to_up(duration=3000)  # 上滑出现设置
-        mp = MysPage(self.driver)  # 实例化"我的"界面
-        mp.click_set()  # 点击设置按钮
-        sleep(1)
-        msp = MysSetPage(self.driver)  # 实例化我的设置界面
-        msp.click_sign_out()  # 点击退出登录
-        hp = HomePage(self.driver)
         self.driver.quit()
